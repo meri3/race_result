c5dfc89bee5fad17bd6f1172945124ab3488ac4e
Meri  Sumic <merisumic@gmail.com>
2022-09-05 23:55:16 +0200

    deleted unnecessary files and parts of code

--- src/Controller/RaceController.php
+++ src/Controller/RaceController.php
@@ -2,198 +2,95 @@
 
 namespace App\Controller;
 
-use App\Entity\Race;
+
 use App\Repository\RaceRepository;
 use App\Repository\ResultRepository;
 use App\Service\CreateDbEntryServiceResult;
-use App\Service\CreateDbEntryServiceRace;
-use App\Service\RaceService;
-use App\Service\ImportCsvService;
-use Doctrine\ORM\Tools\Console\Command\SchemaTool\CreateCommand;
-use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
 
 
 
 class RaceController extends AbstractController
 {
 
-    private RaceService $raceService;
-    private CreateDbEntryServiceRace $createDbEntryServiceRace;
     private CreateDbEntryServiceResult $createDbEntryServiceResult;
     private RaceRepository $raceRepository;
-    private ImportCsvService $importCsvService;
-    private ResultRepository $resultRepository;
+    private RaceRepository $resultRepository;
 
     public function __construct(
-        RaceService $raceService, 
-        CreateDbEntryServiceRace $createDbEntryServiceRace,
+        
         CreateDbEntryServiceResult $createDbEntryServiceResult,
         RaceRepository $raceRepository,
-        ImportCsvService $importCsvService,
-        ResultRepository $resultRepository)
+        RaceRepository $resultRepository
+        )
         
     {
-        $this->raceService = $raceService;
-        $this->createDbEntryServiceRace = $createDbEntryServiceRace;
+       
         $this->createDbEntryServiceResult = $createDbEntryServiceResult;
         $this->raceRepository = $raceRepository;
-        $this->importCsvService = $importCsvService;
         $this->resultRepository = $resultRepository;
-    }
 
+      
+    }
     
     #[Route('/race', name: 'app_race')]
     public function index(): Response
     {
         $files = $this->raceRepository->findAll();
-        // $csv_file = $this->importCsvService->csvImportToMySql();
-        // $csv_file1 = $this->createDbEntryServiceResult->uploadAndInjectCSV();
-
-
-        return $this->render('race/index.html.twig', 
-        
+    
+        return $this->render('race/index.html.twig',        
         [
             'controller_name' => 'RaceController',
             "files"=>$files,
-            // "csv_file" => $csv_file,
-            // "csv_file1" => $csv_file1
+          
 
         ]);
     }
 
-    // #[Route('/race', name: 'csv_upload')]
-    // public function uploadCsv()
-    // {
-    //     $csv_file = $this->importCsvService->csvImportToMySql();
-
-    //     return $this->render('race/index.html.twig',
-    //     [
-    //         "csv_file" => $csv_file
-    //     ]);
-    // }
-
+  
     #[Route('/upload', name: 'upload')]
     public function uploadFile()
     {
         $response = "";
-
-        // if($this->raceService->uploadFile()){
-
-            // $this->createDbEntryServiceRace->createDbEntry();
-            
+       
             $this->createDbEntryServiceResult->uploadAndInjectCSV();
-
-            // $this->createTableService->execute($input, $output);
-
-            return $this->redirect(url:"/results");
-        // } else {
-            // $response = "Error";
-        // }
-        return $response;
-
-    }
-
-    // #[Route('/upload', name: 'upload')]
-    // public function uploadFile()
-    // {
-    //     $response = "";
-
-    //     if($this->raceService->uploadFile()){
-
-    //         $this->createDbEntryServiceRace->createDbEntry();
-    //         $this->createDbEntryServiceRace->createCSVEntry();
-
-    //         // $this->createTableService->execute($input, $output);
-
-    //         return $this->redirect(url:"/results");
-    //     } else {
-    //         $response = "Error";
-    //     }
-    //     return $response;
-
-    // }
-
-    #[Route('/results', name: 'results')]
-    public function readCsvFile()
-    {
-        $response = "";
-
-        if($this->raceService->uploadFile()){
-
-            $this->createDbEntryServiceRace->createDbEntry();
-
-            $this->importCsvService->csvImportToMysql();
-
-            // $this->createTableService->execute($input, $output);
-
-            return $this->redirect(url:"/results");
-        } else {
-            $response = "Error";
-        }
-        return $response;
-
-    }
-
-
-    // #[Route('/results', name: 'results')]
-    // public function seeCSV()
-    // {
-    //     $response = "";
-
-    //     if($this->createDbEntryServiceResult->upload_csv()){
-
-    //         // $this->createTableService->execute($input, $output);
-
-    //         return $response='csv ucitan';
-    //     } else {
-    //         $response = "";
-    //     }
-    //     return $response;
-
-    // }
-    
-
-    // #[Route('/results', name: 'csv_file')]
-    // public function readCSV(): BinaryFileResponse
-    // {
-    //     $csvFile = file('Downloadsresults.csv');
-    //     $data = [];
-    //     foreach ($csvFile as $line){
-    //         $data[] = str_getcsv($line);
             
-    //         return $this-> $csvFile;
-    //     }
+            return $this->redirect(url:"/results");
 
-    //     // return new Response(
-    //     //     'There are no jobs in the database', 
-    //     //     Response::HTTP_ACCEPTED
-    //     // );
-    // }
+        return $response;
+
+    }
     
-   
-
     #[Route('/results', name: 'results')]
     public function results(): Response
     {
         $files = $this->raceRepository->findAll();
-        $file = $this->raceRepository->findAll();
+        
 
         return $this->render('/race/results.html.twig',  [
             'controller_name' => 'RaceController',
-            "files"=>$files,
-            "file"=>$file
+            "files"=>$files
+            
         ]);
     }
- 
 
-    
+
+    #[Route('/results/{id}', methods: ['GET'], name: 'show_results')]
+    public function singleRace($id): Response
+    {
+        $file = $this->raceRepository->find($id);
+        $files = $this->raceRepository->findAll();
+
+        
+        return $this->render('/race/results.html.twig', [
+            'file' => $file,
+            'files'=> $files
+        ]);
+    }
+
     #[Route('/download-file/{id}')]
     public function downloadFile($id): Response
     {
@@ -201,36 +98,5 @@ class RaceController extends AbstractController
 
         return $this->file($_SERVER['DOCUMENT_ROOT']."/Downloads/".$file->getraceName());
     }
-
-    // #[Route('/download-file/{id}')]
-    // public function downloadFile1(): Response
-    // {
-
-    //     // $file = $this->
-
-    //     return $this->render("/race/results.html.twig");
-    // }
-
-
 }
-
-    // protected $request;
-
-
-    // #[Route('/show', name: 'show')]
-    // public function show(RequestStack $request_stack): Response
-    // {
-    //     $request = $request_stack->getCurrentRequest();
-    //     $data = json_decode($request->getContent(), true);
-
-    //     $response = new Response($data);
-    //     $response->headers->set('Content-Type', 'application/json');
-
-    //     return $response;
-    //     return $this->render('race/index.html.twig', [
-    //         'controller_name' => 'RaceController',
-    //     ]);
-    // }
-
-
 
--- src/Service/CreateDbEntryServiceResult.php
+++ src/Service/CreateDbEntryServiceResult.php
@@ -7,12 +7,7 @@ use App\Entity\Race;
 use App\Entity\Result;
 use App\Repository\ResultRepository;
 use App\Repository\RaceRepository;
-use DateTime;
-use Doctrine\ORM\EntityManager;
-use mysqli;
-use Symfony\Component\BrowserKit\Request;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\Validator\Constraints\Regex;
+
 
 class CreateDbEntryServiceResult extends RaceService  
 {
@@ -27,24 +22,11 @@ class CreateDbEntryServiceResult extends RaceService
      }
 
 
-//      public function upload_csv()
-//      {
-//           $display_table =$_REQUEST['csv_file'] ?? null;
-//           return $display_table;
-//      }
-
-// }
-
-
-
 public function uploadAndInjectCSV() 
 {
-     include_once 'db.php';
+    
      $content = $_FILES['csv_file'];
-     // fgetcsv($content);
-     // file_get_contents($_FILES['csv_file']['tmp_name']);
-     // $content = fgetcsv($_FILES['csv_file']['tmp_name']);
-
+    
 
      $race = new Race;
 
@@ -85,44 +67,25 @@ public function uploadAndInjectCSV()
 
                      $fullName = $getData[0];
                      $distance = $getData[1];
-                  
-                     
                      $str_time = $getData[2];
 
+                    
                     $str_time = preg_replace("/^([\d]{1,2})\:([\d]{2})$/", "00:$1:$2", $str_time);
-
                     sscanf($str_time, "%d:%d:%d", $hours, $minutes, $seconds);
-
                     $time_seconds = $hours * 3600 + $minutes * 60 + $seconds;
-
                     $raceTime = $time_seconds ;
 
-                     
+                    
                     $result = new Result;
                     $result ->setRace($race);
                     $result-> setFullName($fullName);
                     $result-> setFinishTime($raceTime);
                     $result -> setDistance($distance);
                     $result-> setPlacement(1); 
+
                     
                     $this->resultRepository->save($result);
-                    // $fullName = $getData[0];
-                    // $distance = $getData[1];
-                    // $raceTime = $getData[2];
-
-
-                    // $query = "SELECT id FROM users WHERE fullName = '" . $getData[0] . "'";
-                    // $check = mysqli_query($conn, $query);
-
-                    // if($check -> num_rows > 0)
-                    // {
-                    //      mysqli_query($conn, "UPDATE RESULT SET full_name = ' " .$fullName. " ' , distance = ' " . $distance . " ', race_time = ' " . $raceTime . " '");
-                    // }            
-                    // else
-                    // {
-                    //      mysqli_query($conn, "(INSERT INTO users (full_name, race_time, distance) VALUES ('" . $fullName. "', '" .$raceTime. "', '" .$distance. "')");                
-                                    
-
+                                                  
                     }
                     
                     fclose($csvFile);
@@ -133,41 +96,3 @@ public function uploadAndInjectCSV()
      }
      
      }
-
-
-
-     // // Form creation ommited
-    
-     // // $form->handleRequest($request);
-     
-     // // if ($form->isSubmitted() && $form->isValid()) { 
-     // //      $file = $form->get('csv_file')->getData();
-          
-     // //      // Opent the file
-     //      if (($handle = fopen($file->getPathName(), "r")) !==false){
-
-     //           // Read and procss the lines
-     //           // Skip the first line if hte file contains a header
-     //           while (($data = fgetcsv($file)) !==false) {
-     //                //Do the processing: Map line to entity, validate if needed
-     //                $result = new Result;
-
-     //                $result -> setFullName($data[0]);
-     //                $result -> setRaceTime($data[1]);
-     //                $result -> setDistance($data[2]);
-     //                $result-> setPlacement($data[3]);
-
-     //                // $em->persist($result);
-     //                $this->resultRepository->save($result);
-
-     //                return $result;
-     //           }
-
-     //           fclose($handle);
-     //           $em->flush();
-               
-     //      }
-
-     
-
-
